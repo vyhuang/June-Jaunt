02c0bbe13e3818b32b380654ce634e18127c7578
Vincent Huang <vincent.yh.huang@gmail.com>
2021-06-14 15:12:52 -0500

    Added SkillFactory to automatically parse JSON data file

--- scripts/skill.gd
+++ scripts/skill.gd
@@ -2,13 +2,70 @@ extends Reference
 
 class_name Skill
 
+var name : String
+
 # Can be 'health' or 'status'
 var type : String
 
 # Can be '0.00, .10, or 0.15'
-var crit_chance : float
+# Can be 'none', 'low', or 'medium'
+var crit_chance : String
 
+# Used to flag whether the target will directly benefit from the action
 var helpful_to_target : bool
 
 # Can be '+/-<status_effect>' or '+/-<numeric_value>'
 var modifier : String
+
+# Only used for 'health' actions. Can be 'precise', 'narrow', 'wide'
+var num_range : String
+
+func _init(skill_name, skill_type, skill_crit_chance, skill_helpful_to_target, 
+           skill_modifier, skill_num_range):
+    self.name = skill_name
+    self.type = skill_type
+    self.crit_chance = skill_crit_chance
+    self.helpful_to_target = skill_helpful_to_target
+    self.modifier = skill_modifier
+    self.num_range = skill_num_range
+
+func skill_name() -> String:
+    return name
+
+func affects_health() -> bool:
+    return type == "health"
+
+func affects_status_conditions() -> bool:
+    return type != "health"
+
+func is_helpful() -> bool:
+    return helpful_to_target
+
+func get_crit_chance() -> float:
+    match (crit_chance):
+        "none":
+            return 0.0
+        "low":
+            return 0.1
+        "medium":
+            return 0.2
+        _:
+            return 0.0
+
+func get_range_modifier() -> float:
+    if (affects_status_conditions()):
+        return 0.0
+
+    match (num_range):
+        "precise":
+            return 0.0
+        "narrow":
+            return 0.2
+        "wide":
+            return 0.35
+        _:
+            return 0.0
+
+func _to_string():
+    var result : String = "[%s; type=%s,crit-chance=%s,helpful=%s,modifier=%s,range=%s]"
+    return result % [self.name, self.type, self.crit_chance, self.helpful_to_target, self.modifier, self.num_range]
---
+++ scripts/skill_factory.gd
@@ -0,0 +1,33 @@
+extends Reference
+
+class_name SkillFactory
+
+# what am I doing, geez.
+const Skill = preload("res://scripts/skill.gd")
+var skills
+
+func get_all_skills() -> Array:
+    if skills == null:
+        skills = parse_skills()
+    return skills
+
+func parse_skills() -> Dictionary:
+    var result : Dictionary = {}
+
+    var file : File = File.new()
+    var file_result = file.open("res://resources/skills.json", File.READ)
+    if file_result != OK:
+        push_error("Could not open res://resources/skills.json")
+        return result
+
+    var jsonResult = JSON.parse(file.get_as_text())
+    if typeof(jsonResult.result) == TYPE_DICTIONARY:
+        var parsed_skills = jsonResult.result["skills"] as Array
+        for parsed_skill in parsed_skills:
+            var skill : Skill = Skill.new(parsed_skill.name, parsed_skill.type, 
+                    parsed_skill.crit_chance, parsed_skill.helpful_to_target,
+                    parsed_skill.modifier, parsed_skill.num_range) 
+            result[skill.skill_name()] = skill
+    else:
+        push_error("could not parse res://resources/skills.json")
+    return result
