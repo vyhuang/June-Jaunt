ae426c3d89f08019fd5df6f6c3d9442051ca46ac
Vincent Huang <vincent.yh.huang@gmail.com>
2021-06-14 12:32:35 -0500

    updated skills + status conditions

--- scripts/battle_constants.gd
+++ scripts/battle_constants.gd
@@ -6,6 +6,45 @@ enum BATTLE_STATE {
     IN_BATTLE, PAUSED, VICTORY, DEFEAT}
 
 enum STATUS {
+    # Cannot be healed or take any actions. If all units in one side of the 
+    # battle are unconscious the battle ends.
     UNCONSCIOUS, 
-    BLEEDING, BLINDED, STUNNED, WEAKENED, 
-    MENDING, SPIRITED}
+
+    # Takes damage over for a set period of time. The amount of health
+    # lost depends on the afflicted's stats.
+    BLEEDING, 
+    # Introduces chance of actions missing their intended target. Missed 
+    # actions have a chance to hit another random target. Will wear off after 
+    # a short amount of time.
+    BLINDED, 
+    # The next hit of damage taken will be multiplied by 1.5.
+    EXPOSED,
+    # Interrupts and prevents actions from being taken. Will wear off after a
+    # short amount of time.
+    STUNNED, 
+    # Energy regen is halved. How long this status lasts depends on the
+    # afflicted's stats.
+    WEAKENED, 
+
+    # Reduces the effects of the next negative action taken against the 
+    # recipient. Damage will be reduced by 25%, and status conditions will
+    # have a 25% chance of not being applied. Will also restore an amount
+    # of energy based on the recipient's stats.
+    BRACED,
+    # Raises the critical chance of actions by one tier. Critical successes
+    # for 'health' actions will apply a multiplier of 1.5. Critical successes
+    # for 'status' actions will apply a multiplier of to the applied status' 
+    # duration.
+    FOCUSED,
+    # Nullifies the next negative action taken against the recipient. Removes
+    # the 'EXPOSED' status, but all other status conditions remain.
+    GUARDED,
+    # Regenerates health for a set amount of time. How much health is 
+    # regenerated depends on the recipient's stats.
+    MENDING, 
+    # Energy regen is multiplied by 1.75. How long this status lasts depends
+    # on the recipient's stats.
+    SPIRITED,
+    # Removes all negative status effects and prevents any others from being
+    # applied for a set amount of time.
+    WARDED}
--- scripts/skill.gd
+++ scripts/skill.gd
@@ -8,5 +8,7 @@ var type : String
 # Can be '0.00, .10, or 0.15'
 var crit_chance : float
 
+var helpful_to_target : bool
+
 # Can be '+/-<status_effect>' or '+/-<numeric_value>'
 var modifier : String
